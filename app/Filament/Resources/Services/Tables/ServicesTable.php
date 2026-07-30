a6daa6d86465905ef15c2a45403fb460716b6dc9
daze7X <kopinih91@gmail.com>
2026-07-30 08:21:28 +0700

    deploy: restore all table resources to correct Filament Actions namespaces

--- app/Filament/Resources/Services/Tables/ServicesTable.php
+++ app/Filament/Resources/Services/Tables/ServicesTable.php
@@ -2,10 +2,10 @@
 
 namespace App\Filament\Resources\Services\Tables;
 
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\DeleteAction; // Aksi hapus satuan
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction; // Aksi hapus satuan
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
