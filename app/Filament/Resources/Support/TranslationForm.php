90ecbbbeeb69dd7bd9878a3a5005d8e92b01be67
daze7X <kopinih91@gmail.com>
2026-08-06 06:54:14 +0700

    fix(filament): update Section namespace to Filament\Schemas\Components\Section

--- app/Filament/Resources/Support/TranslationForm.php
+++ app/Filament/Resources/Support/TranslationForm.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Resources\Support;
 
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\MarkdownEditor;
