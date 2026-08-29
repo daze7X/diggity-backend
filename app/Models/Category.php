c16d2b634c2b0b306aabfe186e47e93fd6d5a804
daze7X <kopinih91@gmail.com>
2026-08-29 09:52:22 +0700

    fix: correct Category.php syntax

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -16,20 +16,15 @@ class Category extends Model
     public function parent()
     {
         return $this->belongsTo(Category::class, 'parent_id');
-    
-    public function products()
-    {
-        return $this->hasMany(Product::class, 'category_id');
     }
-}
 
     public function children()
     {
         return $this->hasMany(Category::class, 'parent_id');
-    
+    }
+
     public function products()
     {
         return $this->hasMany(Product::class, 'category_id');
     }
-}
 }
